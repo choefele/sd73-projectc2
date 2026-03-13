c07c2cbc4d3acc849af66ce96b21bd08c91575d5
Claus Höfele <claus@claushoefele.com>
2026-03-13 11:44:21 +0100

    Return enum for play

--- src/rock-paper-scissors.ts
+++ src/rock-paper-scissors.ts
@@ -4,26 +4,32 @@ enum Move {
   Scissors,
 }
 
+enum Outcome {
+  Win,
+  Lose,
+  Tie,
+}
+
 function generateMove(): Move {
   const numberOfMoves = Object.keys(Move).length / 2;
   const index = Math.floor(Math.random() * numberOfMoves);
   return Move[Move[index] as keyof typeof Move];
 }
 
-function play(player1: Move, player2: Move): boolean | undefined {
+function play(player1: Move, player2: Move): Outcome {
   if (player1 === Move.Rock) {
-    if (player2 === Move.Scissors) return true;
-    if (player2 === Move.Paper) return false;
+    if (player2 === Move.Scissors) return Outcome.Win;
+    if (player2 === Move.Paper) return Outcome.Lose;
   } else if (player1 === Move.Paper) {
-    if (player2 === Move.Rock) return true;
-    if (player2 === Move.Scissors) return false;
+    if (player2 === Move.Rock) return Outcome.Win;
+    if (player2 === Move.Scissors) return Outcome.Lose;
   } else {
     // Move.Scissors
-    if (player2 === Move.Paper) return true;
-    if (player2 === Move.Rock) return false;
+    if (player2 === Move.Paper) return Outcome.Win;
+    if (player2 === Move.Rock) return Outcome.Lose;
   }
 
-  return undefined;
+  return Outcome.Tie;
 }
 
-export { generateMove, play, Move };
+export { generateMove, play, Move, Outcome };
--- tests/rock-paper-scissors.test.ts
+++ tests/rock-paper-scissors.test.ts
@@ -1,5 +1,10 @@
 import { describe, expect, it } from "vitest";
-import { generateMove, play, Move } from "../src/rock-paper-scissors.js";
+import {
+  generateMove,
+  play,
+  Move,
+  Outcome,
+} from "../src/rock-paper-scissors.js";
 
 describe("generateMove", () => {
   it("generates random move", () => {
@@ -10,29 +15,29 @@ describe("generateMove", () => {
 
 describe("play", () => {
   it("rock beats scissors", () => {
-    expect(play(Move.Rock, Move.Scissors)).toBeTruthy();
+    expect(play(Move.Rock, Move.Scissors)).toBe(Outcome.Win);
   });
   it("rock loses to paper", () => {
-    expect(play(Move.Rock, Move.Paper)).toBeFalsy();
+    expect(play(Move.Rock, Move.Paper)).toBe(Outcome.Lose);
   });
 
   it("paper loses to scissors", () => {
-    expect(play(Move.Paper, Move.Scissors)).toBeFalsy();
+    expect(play(Move.Paper, Move.Scissors)).toBe(Outcome.Lose);
   });
   it("paper beats rock", () => {
-    expect(play(Move.Paper, Move.Rock)).toBeTruthy();
+    expect(play(Move.Paper, Move.Rock)).toBe(Outcome.Win);
   });
 
   it("scissors loses to rock", () => {
-    expect(play(Move.Scissors, Move.Rock)).toBeFalsy();
+    expect(play(Move.Scissors, Move.Rock)).toBe(Outcome.Lose);
   });
   it("scissors beats paper", () => {
-    expect(play(Move.Scissors, Move.Paper)).toBeTruthy();
+    expect(play(Move.Scissors, Move.Paper)).toBe(Outcome.Win);
   });
 
-  it("moves must be different", () => {
-    expect(play(Move.Rock, Move.Rock)).toBeUndefined();
-    expect(play(Move.Scissors, Move.Scissors)).toBeUndefined();
-    expect(play(Move.Paper, Move.Paper)).toBeUndefined();
+  it("same move ends in a tie", () => {
+    expect(play(Move.Rock, Move.Rock)).toBe(Outcome.Tie);
+    expect(play(Move.Scissors, Move.Scissors)).toBe(Outcome.Tie);
+    expect(play(Move.Paper, Move.Paper)).toBe(Outcome.Tie);
   });
 });
