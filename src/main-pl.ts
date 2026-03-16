b5e516cd3efcd5d0317aa546d98b35bf6e483964
Claus Höfele <claus@claushoefele.com>
2026-03-16 14:23:48 +0100

    Finalize pl command

--- src/main-pl.ts
+++ src/main-pl.ts
@@ -1,6 +1,11 @@
 import { translate } from "./pig-latin.js";
 import { MainOutput } from "./main.js";
 
-export function pl(moveInput: string): MainOutput {
-  return { exitCode: 0, output: "" };
+export function pl(text: string): MainOutput {
+  let output = "";
+
+  output += `Input: ${text}\n`;
+  output += `Translation: ${translate(text)}\n`;
+
+  return { exitCode: 0, output };
 }
